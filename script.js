26098d2c94ef2f90c3451c666c3dadb2fcf518a7
Bassem Sherief <108496148+BassemAdam@users.noreply.github.com>
2024-03-18 02:19:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,24 @@ $(document).ready(function(){
         items:3,
         loop:true,
         margin:10,
+        responsiveClass:true,
         nav: true,
         autoplay: true,
         autoplayTimeout: 2000,
         startPosition: carousel.children().length / 2 - 1,
         responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
             600:{
-                items:3
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:true
             }
         }
     });
